3ba6711bb0da05983aff9381db64473c9720dc78
zmaniacz <zz@redial.net>
2015-05-28 16:11:04 -0700

    test fix

--- app/webroot/parser/pdfparse.sh
+++ app/webroot/parser/pdfparse.sh
@@ -2,14 +2,15 @@
 
 if [ $# = 1 ]; then
 	DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+	cd $DIR
 	echo "InputDirectory=$DIR/incoming/$1/
-OutputDirectory=$DIR/output/$1/" > $DIR/LFScoreParser.properties
-	mkdir -p $DIR/output/$1/
-	mkdir -p $DIR/pending/$1/
-	java -Xmx100M -jar $DIR/LFScoreParser.jar
-	mv $DIR/output/$1/*.pdf $DIR/../pdf/
-	mv $DIR/output/$1/*.csv $DIR/pending/$1/
-	rm $DIR/output/$1/*
+OutputDirectory=$DIR/output/$1/" > LFScoreParser.properties
+	mkdir -p output/$1/
+	mkdir -p pending/$1/
+	java -Xmx100M -jar LFScoreParser.jar
+	mv output/$1/*.pdf ../pdf/
+	mv output/$1/*.csv pending/$1/
+	rm output/$1/*
 	exit 0
 else
 	exit 1
