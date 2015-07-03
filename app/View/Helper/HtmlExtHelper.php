287b5c45e57afbafbb117f3329dc22f7fdeba2df
zmaniacz <zz@redial.net>
2015-07-02 23:26:00 -0700

    Updated helper to ovverride url() and link() to add the querystring based on current app state

--- app/View/Helper/HtmlExtHelper.php
+++ app/View/Helper/HtmlExtHelper.php
@@ -2,12 +2,23 @@
 	App::uses('HtmlHelper', 'View/Helper');
 	
 	class HtmlExtHelper extends HtmlHelper {
+		var $helpers = array('Session');
+		
 		public function link($title, $url = null, $options = array(), $confirmMessage = false) {
-			if(!is_array($url))
-				$url = array($url);
+			if(is_array($url) && empty($url['?'])) {
+				$querystring = array('?' => array('gametype' => $this->Session->read('state.gametype'), 'leagueID' => $this->Session->read('state.leagueID'), 'centerID' => $this->Session->read('state.centerID')));
+				$url = $url + $querystring;
+			}
 			
-			$new_url = $url + array('?' => array('gametype' => 'all'));
-			
-			return parent::link($title, $new_url, $options, $confirmMessage);
+			return parent::link($title, $url, $options, $confirmMessage);
+		}
+		
+		public function url($url = null, $full = false) {
+			if(is_array($url) && empty($url['?'])) {
+				$querystring = array('?' => array('gametype' => $this->Session->read('state.gametype'), 'leagueID' => $this->Session->read('state.leagueID'), 'centerID' => $this->Session->read('state.centerID')));
+				$url = $url + $querystring;
+			}
+
+			return parent::url($url, $full);
 		}
 	}
