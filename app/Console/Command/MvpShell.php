0563c0d6b93e446216dba403677ebda0fca3e988
zmaniacz <zz@redial.net>
2016-05-25 00:50:45 -0700

    Adde din new Yama model for testing

--- app/Console/Command/MvpShell.php
+++ app/Console/Command/MvpShell.php
@@ -2,6 +2,9 @@
 class MvpShell extends AppShell {
     public $uses = array('Scorecard');
     public function main() {
-        $this->Scorecard->generateMVP();
+        $this->Scorecard->generateMVP(0,500);
+        $this->Scorecard->generateMVP(500,500);
+        $this->Scorecard->generateMVP(1000,500);
+        $this->Scorecard->generateMVP(1500,500);
     }
 }
