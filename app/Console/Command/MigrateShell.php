b947b31b930f83d05015bc02b8fa3e8752d328d2
zmaniacz <zz@redial.net>
2017-01-27 14:47:56 -0800

    Don't actually need the player_teams table at all...kill it with fire

--- app/Console/Command/MigrateShell.php
+++ app/Console/Command/MigrateShell.php
@@ -18,26 +18,8 @@ class MigrateShell extends AppShell {
                             FOREIGN KEY (`league_id`)
                             REFERENCES `lfstats`.`leagues` (`id`)");
         
-        //rename player_teams table
-        $db->rawQuery("ALTER TABLE `lfstats`.`players_teams` RENAME TO  `lfstats`.`players_league_teams`");
-        //change column name and rename foreign keys
-        $db->rawQuery("ALTER TABLE `lfstats`.`players_league_teams` DROP FOREIGN KEY `fk_players_teams_teams_team_id`");
-        $db->rawQuery("ALTER TABLE `lfstats`.`players_league_teams` CHANGE COLUMN `team_id` `league_team_id` INT(11) NOT NULL");
-        $db->rawQuery("ALTER TABLE `lfstats`.`players_league_teams` 
-                        ADD CONSTRAINT `fk_players_teams_teams_team_id`
-                            FOREIGN KEY (`league_team_id`)
-                            REFERENCES `lfstats`.`league_teams` (`id`)");
-        //rename remaining FKs
-        $db->rawQuery("ALTER TABLE `lfstats`.`players_league_teams` 
-                        DROP FOREIGN KEY `fk_players_teams_players_player_id`,
-                        DROP FOREIGN KEY `fk_players_teams_teams_team_id`");
-        $db->rawQuery("ALTER TABLE `lfstats`.`players_league_teams` 
-                        ADD CONSTRAINT `fk_players_league_teams_players_player_id`
-                            FOREIGN KEY (`player_id`)
-                            REFERENCES `lfstats`.`players` (`id`),
-                        ADD CONSTRAINT `fk_players_league_teams_teams_team_id`
-                            FOREIGN KEY (`league_team_id`)
-                            REFERENCES `lfstats`.`league_teams` (`id`)");
+        //drop player_teams table -- unused
+        $db->rawQuery("DROP TABLE `lfstats`.`players_teams`;");
 
         //create the new teams table
         $db->rawQuery("CREATE TABLE `teams` (
@@ -50,11 +32,14 @@ class MigrateShell extends AppShell {
                         `eliminated` tinyint(1) NOT NULL DEFAULT '0',
                         `eliminated_opponent` tinyint(1) NOT NULL DEFAULT '0',
                         `game_id` int(11) NOT NULL,
+                        `league_team_id' int(11) DEFAULT NULL,
                         `created` datetime NULL DEFAULT NULL,
                         `updated` datetime NULL DEFAULT NULL,
                         PRIMARY KEY (`id`),
                         KEY `game_id_idx` (`game_id`),
-                        CONSTRAINT `fk_teams_games_game_id` FOREIGN KEY (`game_id`) REFERENCES `games` (`id`)
+                        KEY `league_team_id_idx` (`league_team_id`), 
+                        CONSTRAINT `fk_teams_games_game_id` FOREIGN KEY (`game_id`) REFERENCES `games` (`id`),
+                        CONSTRAINT `fk_teams_league_teams_league_team_id` FOREIGN KEY (`league_team_id`) REFERENCES `league_teams` (`id`),
                     ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci");
 
         //create events table
@@ -81,8 +66,7 @@ class MigrateShell extends AppShell {
 
         //remove redundant game data - kill columns
 
-        
-
+        //add fk from games to events
         //create event linkages
     }
 }
