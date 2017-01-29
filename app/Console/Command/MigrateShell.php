6d33e1a66f4b9610f13bcd7674481fb648c9487c
zmaniacz <zz@redial.net>
2017-01-28 22:29:11 -0800

    DB migration script mostly complete

--- app/Console/Command/MigrateShell.php
+++ app/Console/Command/MigrateShell.php
@@ -3,6 +3,8 @@
 App::uses('ConnectionManager', 'Model');
 
 class MigrateShell extends AppShell {
+    public $uses = array('Team','Game','Scorecard');
+
     public function main() {
         $this->out('choose a step');
     }
@@ -52,7 +54,43 @@ class MigrateShell extends AppShell {
                         PRIMARY KEY (`id`)
                     ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci");
         
-        //add team fk to scorecards
+        //add fk from games to events
+        //create event linkages
+    }
+
+    public function step_2() {
+        $db = ConnectionManager::getDataSource('default');
+
+        //create teams records based on existing game records
+        //select all the games with their red team data
+        $games = $this->Game->find('all');
+        foreach($games as $game) {
+            $teams = array(
+                array(
+                    'color' => 'red',
+                    'raw_score' => $game['Game']['red_score'],
+                    'winner' => ($game['Game']['winner'] == 'red' ? 1 : 0),
+                    'eliminated' => $game['Game']['red_eliminated'],
+                    'eliminated_opponent' => $game['Game']['green_eliminated'],
+                    'game_id' => $game['Game']['id'],
+                    'league_team_id' => $game['Game']['red_team_id']
+                ),
+                array(
+                    'color' => 'green',
+                    'raw_score' => $game['Game']['green_score'],
+                    'winner' => ($game['Game']['winner'] == 'green' ? 1 : 0),
+                    'eliminated' => $game['Game']['green_eliminated'],
+                    'eliminated_opponent' => $game['Game']['red_eliminated'],
+                    'game_id' => $game['Game']['id'],
+                    'league_team_id' => $game['Game']['green_team_id']
+                )
+            );
+
+            $this->Team->saveMany($teams);
+            $this->Team->clear();
+        }
+
+        //add team_id fk to scorecards
         $db->rawQuery("ALTER TABLE `lfstats`.`scorecards` 
                         ADD COLUMN `team_id` INT(11) NULL DEFAULT NULL AFTER `league_id`");
         $db->rawQuery("ALTER TABLE `lfstats`.`scorecards` 
@@ -61,20 +99,100 @@ class MigrateShell extends AppShell {
                         ADD CONSTRAINT `fk_scorecards_teams_team_id`
                             FOREIGN KEY (`team_id`)
                             REFERENCES `lfstats`.`teams` (`id`)");
-        
-        //create teams records based on existing game records
+    }
 
-        //select all the games with their red team data
-        //insert into teams with the game_id as red data
-        //dont use adj scores...add elim bonuses as appropriate
-        //have to go back after scorecards are tied out and re-apply penalties
-        //repeat for green
-
+    public function step_3() {
         //link scorecards to teams instead of games
+        $scorecards = $this->Scorecard->find('all', array('fields' => array('id', 'team', 'game_id', 'team_id')));
+        $games = $this->Game->find('all', array(
+            'fields' => array('Game.id'),
+			'contain' => array(
+                'Team' =>array(
+                    'id',
+                    'color'
+                )
+            )
+        ));
 
+        $results = array();
+        array_map(function($n) use (&$results) {
+            $results[$n['Game']['id']] = array(
+                $n['Team'][0]['color'] => $n['Team'][0]['id'],
+                $n['Team'][1]['color'] => $n['Team'][1]['id']
+            );
+            return null;
+        }, $games);
+
+        foreach($scorecards as $scorecard) {
+            $scorecard['Scorecard']['team_id'] = $results[$scorecard['Scorecard']['game_id']][$scorecard['Scorecard']['team']];
+            $this->Scorecard->save($scorecard);
+            $this->Scorecard->clear();
+        }
+    }
+
+    public function step_4() {
         //remove redundant game data - kill columns
+        $db = ConnectionManager::getDataSource('default');
 
-        //add fk from games to events
-        //create event linkages
+        $db->rawQuery("ALTER TABLE `lfstats`.`games` 
+                        DROP FOREIGN KEY `fk_games_centers_center_id`,
+                        DROP FOREIGN KEY `fk_games_teams_red_team_id`,
+                        DROP FOREIGN KEY `fk_games_teams_green_team_id`");
+
+        $db->rawQuery("ALTER TABLE `lfstats`.`games` 
+                        DROP COLUMN `green_eliminated`,
+                        DROP COLUMN `red_eliminated`,
+                        DROP COLUMN `winner`,
+                        DROP COLUMN `green_adj`,
+                        DROP COLUMN `red_adj`,
+                        DROP COLUMN `green_score`,
+                        DROP COLUMN `red_score`,
+                        DROP COLUMN `green_team_name`,
+                        DROP COLUMN `red_team_name`,
+                        DROP COLUMN `red_team_id`,
+                        DROP COLUMN `green_team_id`,
+                        CHANGE COLUMN `game_name` `game_name` VARCHAR(100) CHARACTER SET 'utf8' NULL DEFAULT NULL ,
+                        CHANGE COLUMN `game_description` `game_description` TEXT CHARACTER SET 'utf8' NULL DEFAULT NULL ,
+                        CHANGE COLUMN `center_id` `center_id` INT(11) NOT NULL ,
+                        DROP INDEX `red_team_id` ,
+                        DROP INDEX `green_team_id`");
+
+        $db->rawQuery("ALTER TABLE `lfstats`.`games` 
+                        ADD CONSTRAINT `fk_games_centers_center_id`
+                        FOREIGN KEY (`center_id`)
+                        REFERENCES `lfstats`.`centers` (`id`)
+                        ON DELETE NO ACTION
+                        ON UPDATE NO ACTION");
+    }
+
+    public function step_5() {
+        $db = ConnectionManager::getDataSource('default');
+        
+        $db->rawQuery("CREATE OR REPLACE 
+                        ALGORITHM = UNDEFINED 
+                        DEFINER = `dbo_redial`@`%` 
+                        SQL SECURITY DEFINER
+                    VIEW `game_results` AS
+                        SELECT 
+                            `scorecards`.`game_datetime` AS `game_datetime`,
+                            `scorecards`.`player_id` AS `player_id`,
+                            `scorecards`.`id` AS `scorecard_id`,
+                            `games`.`id` AS `game_id`,
+                            `games`.`type` AS `type`,
+                            `games`.`center_id` AS `center_id`,
+                            `games`.`league_id` AS `league_id`,
+                            (CASE `teams`.`winner`
+                                WHEN 1 THEN 'W'
+                                ELSE 'L'
+                            END) AS `result`,
+                            `teams`.`winner` AS `won`
+                        FROM
+                            ((`scorecards`
+                            JOIN `teams` ON ((`scorecards`.`team_id` = `teams`.`id`)))
+                            JOIN `games` ON ((`teams`.`game_id` = `games`.`id`)))");
+    }
+
+    public function step_6() {
+        //create and link events
     }
 }
