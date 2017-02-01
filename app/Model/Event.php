ff4d3e25bfa7b04346d50dbd2d03ae5c2be87235
zmaniacz <zz@redial.net>
2017-01-31 23:50:38 -0800

    Added base Event model

---
+++ app/Model/Event.php
@@ -0,0 +1,57 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Event Model
+ *
+ * @property Center $Center
+ * @property Game $Game
+ */
+class Event extends AppModel {
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'name';
+
+
+	// The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * belongsTo associations
+ *
+ * @var array
+ */
+	public $belongsTo = array(
+		'Center' => array(
+			'className' => 'Center',
+			'foreignKey' => 'center_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+
+/**
+ * hasMany associations
+ *
+ * @var array
+ */
+	public $hasMany = array(
+		'Game' => array(
+			'className' => 'Game',
+			'foreignKey' => 'event_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		)
+	);
+
+}
