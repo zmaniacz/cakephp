2313836d2ab456df7a2a547948a73cd6f3aadc57
Russell Lewis <russell.lewis@outlook.com>
2017-04-25 09:34:25 -0700

    Added AKL to routes

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -27,6 +27,7 @@
 	Router::connect('/syr', array('controller' => 'scorecards', 'action' => 'setState', 'social', null, 8));
 	Router::connect('/ltc', array('controller' => 'scorecards', 'action' => 'setState', 'social', null, 14));
 	Router::connect('/cv', array('controller' => 'scorecards', 'action' => 'setState', 'social', null, 15));
+	Router::connect('/akl', array('controller' => 'scorecards', 'action' => 'setState', 'social', null, 16));
 	Router::connect('/geddon2014', array('controller' => 'scorecards', 'action' => 'setState', 'social', null, 3));
 	Router::connect('/geddon2015', array('controller' => 'scorecards', 'action' => 'setState', 'social', null, 9));
 	Router::connect('/internationals-2014', array('controller' => 'scorecards', 'action' => 'setState', 'league', 7, 5));
